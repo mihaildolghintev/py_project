d39cfb1a810fc07a41339ffc459cc4be24bebc5a
mihail.dolghintev <mihail.dolghintev@saltedge.com>
2024-11-30 00:56:46 +0200

    initialize FastAPI project with basic structure and configurations

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_index():
+    return {"Hello": "World"}
+
+
+@app.get("/api/v1/hello-world/")
+def read_hello_world():
+    return {"what": "road", "where": "kubernetes", "version": "v1"}
